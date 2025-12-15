6684770399c46223bf86b99012aee53272852221
qasimhesen <154603400+qasimhesen@users.noreply.github.com>
2025-12-15 10:00:37 +0400

    Update app.py
    second commit

--- app.py
+++ app.py
@@ -58,5 +58,7 @@ def index():
 
     return render_template_string(HTML)
 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
